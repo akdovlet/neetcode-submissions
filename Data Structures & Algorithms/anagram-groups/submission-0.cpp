fc868b261d68e0cdbbe4ccebe531fbc523184c53
AK <akdovlet@student.42.fr>
2026-06-12 15:43:11 +0200

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    bool                    isAnagram(const std::string &s, const std::string &t)
+    {
+        if (s.size() != t.size())
+            return (false);
+        int arr[26] = {};
+        int arr2[26] = {};
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            ++arr[s[i] - 'a'];
+            ++arr2[t[i] - 'a'];
+        }
+        for (int i = 0; i < 26; i++)
+        {
+            if (arr[i] != arr2[i])
+                return (false);
+        }
+        return (true);
+    }
+
+    vector<vector<string>>  groupAnagrams(vector<string>& strs)
+    {
+        std::vector<vector<string>> result;
+        int                         seen = 0;
+        for (auto i: strs)
+        {
+            for (auto j = 0; j < result.size(); j++)
+            {
+                if (result[j].size() && isAnagram(i, result[j][0]))
+                {
+                    result[j].push_back(i);
+                    seen = 1;
+                    break ;
+                }
+            }
+            if (!seen)
+                result.push_back(std::vector<string>{i});
+            else
+                seen = 0;
+        }
+        return (result);
+    }
+};
