1ecd7ed8c68f9d26d572b0ee3ee596ec60a586ec
AK <akdovlet@student.42.fr>
2026-07-10 17:02:54 +0200

    Add: insertionSort - submission-1

---
+++ Data Structures & Algorithms/insertionSort/submission-1.cpp
@@ -0,0 +1,33 @@
+// Definition for a Pair
+// class Pair {
+// public:
+//     int key;
+//     string value;
+//
+//     Pair(int key, string value) : key(key), value(value) {}
+// };
+class Solution {
+public:
+    vector<vector<Pair>> insertionSort(vector<Pair>& pairs) 
+    {
+        vector<vector<Pair>> result;
+        if (pairs.empty()) 
+            return result;
+        result.push_back(pairs);
+        int i = 1;
+        while (i < pairs.size())
+        {
+            Pair x = pairs[i];
+            int j = i;
+            while (j > 0 && pairs[j - 1].key > x.key)
+            {
+                swap(pairs[j], pairs[j - 1]);
+                j--;
+            }
+            pairs[j] = x;
+            i++;
+            result.push_back(pairs);
+        }
+        return (result);
+    }
+};
